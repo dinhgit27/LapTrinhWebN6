34ad7c5cede2e76f473d20feae51cf7197ab33a3
Nguyễn Đỉnh <dinhnguyen.27062005@gmail.com>
2026-01-02 22:04:16 +0700

    model sua

--- Models/OrderDetail.cs
+++ Models/OrderDetail.cs
@@ -14,13 +14,21 @@ namespace FashionEcommerce.Models
         public int ProductVariantId { get; set; }
 
         [Required]
+        [StringLength(200)]
+        public string Snapshot_ProductName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Snapshot_Sku { get; set; }
+
+        [StringLength(500)]
+        public string Snapshot_Thumbnail { get; set; }
+
+        [Required]
         public int Quantity { get; set; }
 
         [Required]
         public decimal UnitPrice { get; set; }
-
-        [Required]
-        public decimal TotalPrice { get; set; }
 
         // Navigation properties
         public virtual Order Order { get; set; }
